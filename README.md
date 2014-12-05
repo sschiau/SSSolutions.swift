d185ae7e60e9ad66bfc6d15dff987dbf355bb450
Silviu Schiau <pr@silviu.co>
2014-12-05 02:21:59 -0500

    Added Anagram
    Test for Anagram

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Swift solutions to diverse problems. **Playground** implementation.
 **[SSSolutions-Swift-NSRangesIntersect.playground](https://github.com/sschiau/SSSolutions-Swift/blob/master/SSSolutions-Swift-NSRangesIntersect.playground/section-1.swift)**  
 Apple method and Manual method to determine whether two NSRanges intersect
 
+**[SSSolutions-Swift-Anagram.playground](https://github.com/sschiau/SSSolutions-Swift/blob/master/SSSolutions-Swift-Anagram.playground/section-1.swift)**  
+Test for Anagram
+
 ***
 
 #### License
---
+++ SSSolutions-Swift-Anagram.playground/section-1.swift
@@ -0,0 +1,100 @@
+/**
+* Copyright 2014 Silviu Schiau.
+*
+*
+* Anagram
+*
+* Author: Silviu Schiau <pr@silviu.co>
+* Version: 1.0.0
+* License Apache License Version 2.0 http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+* This copyright notice
+* shall be included in all copies or substantial portions of the software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*
+*/
+
+import UIKit
+
+let word = "kjf"
+let string = "djfiakdef"
+
+/*
+*
+* Method n^2
+*
+*/
+func isAnagram(word: String, string: String) -> Bool
+{
+    // Count the word chars matches
+    var count = 0
+    
+    // Iterate all word's chars
+    for charWord in word
+    {
+        var isFound = false
+        
+        // Iterate all string's chars
+        for charString in string
+        {
+            if charWord == charString
+            {
+                // flag true if char is found
+                isFound = true
+            }
+        }
+        
+        if isFound
+        {
+            ++count
+            
+            if count == countElements(word)
+            {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+    
+    return false
+}
+
+/*
+*
+* Method ((n log n ) * 2) + n
+*
+*/
+func isAnagramWithSort(word: String, string: String) -> Bool
+{
+    let sortedWord = sorted(word)
+    let sortedString = sorted(string)
+    
+    // Count matches
+    var count = 0
+    
+    for charWord in sortedWord
+    {
+        if var index = find(sortedString, charWord)
+        {
+            ++count
+            
+            if count == countElements(word)
+            {
+                return true
+            }
+        }
+    }
+    
+    return false
+}
+
+isAnagram(word, string)
+isAnagramWithSort(word, string)
